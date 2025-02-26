48978355eb54b3add67895af3dc849308ab3b593
Dracilus <mungo.lennon@students.mq.edu.au>
2025-02-26 18:12:50 +1100

    Imperfect solution added, will look for more efficient solutions

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -7,10 +7,12 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   vec2 v = abs(p - vec2(0.5, 0.5));
-   float d = max(v.x, v.y); 
+   //vec2 v = abs(p - vec2(0.5, 0.5));
+   //float d = max(v.x, v.y); 
+  float e = abs(p.y-p.x);
+  float r = p.y+p.x;
   
-   if (d < 0.3) {
+   if (r < 1.5 && r > 0.5 && e < 0.5) {
       o_colour = vec4(u_colour.rgb, 1);
    }
    else {
